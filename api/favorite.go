720ac3a85f4fa785b0b629f720fcad9e95bdc1f5
beakerbot <beakerbot@users.noreply.github.com>
2019-08-07 22:55:50 +0000

    Automated update to 30934e26b4cd67a61bb2913aa03d2c42041babeb

--- api/favorite.go
+++ api/favorite.go
@@ -1,10 +1,17 @@
 package api
 
+import "time"
+
 type FavoritePage struct {
 	Data       []Favorite `json:"data"`
 	NextCursor string     `json:"nextCursor,omitempty"`
 }
 
 type Favorite struct {
-	ID string `json:"id"`
+	ID           string    `json:"id"`
+	Owner        Identity  `json:"owner"`
+	Created      time.Time `json:"created"`
+	Description  string    `json:"description,omitempty"`
+	Name         string    `json:"name,omitempty"`
+	FavoriteType string    `json:"type"`
 }
