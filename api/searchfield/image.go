838a66c925a5e3ca19bcb6e86895dcffb20641b9
beakerbot <beakerbot@users.noreply.github.com>
2019-08-23 20:19:18 +0000

    Automated update to 8f409117f520e95777f72032679f673988081a28

--- api/searchfield/image.go
+++ api/searchfield/image.go
@@ -5,6 +5,7 @@ type Image string
 const (
 	ImageID           Image = "id"
 	ImageName         Image = "name"
+	ImageCommitted    Image = "committed"
 	ImageDescription  Image = "description"
 	ImageCreatingUser Image = "user"
 )
