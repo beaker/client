e3835e17df80cb5c1ecf98caf7e5ecf8af9dd27e
beakerbot <beakerbot@users.noreply.github.com>
2019-09-04 22:45:40 +0000

    Automated update to d692774f3449e0a6677a1947f10961afc56e9c57

--- client/workspace.go
+++ client/workspace.go
@@ -3,7 +3,9 @@ package client
 import (
 	"context"
 	"net/http"
+	"net/url"
 	"path"
+	"strconv"
 
 	"github.com/beaker/client/api"
 )
@@ -14,7 +16,10 @@ type WorkspaceHandle struct {
 	// TODO: Support name-based references too when API support is available
 }
 
-func (c *Client) CreateWorkspace(ctx context.Context, spec api.WorkspaceSpec) (*WorkspaceHandle, error) {
+func (c *Client) CreateWorkspace(
+	ctx context.Context,
+	spec api.WorkspaceSpec,
+) (*WorkspaceHandle, error) {
 	resp, err := c.sendRequest(ctx, http.MethodPost, "/api/v3/workspaces", nil, spec)
 	if err != nil {
 		return nil, err
@@ -27,6 +32,40 @@ func (c *Client) CreateWorkspace(ctx context.Context, spec api.WorkspaceSpec) (*
 	}
 
 	return &WorkspaceHandle{client: c, id: workspaceID.ID}, nil
+}
+
+type ListWorkspaceOptions struct {
+	Archived *bool
+	Cursor   string
+}
+
+func (c *Client) ListWorkspaces(
+	ctx context.Context,
+	org string, opts *ListWorkspaceOptions,
+) ([]api.Workspace, string, error) {
+	if opts == nil {
+		opts = &ListWorkspaceOptions{}
+	}
+
+	query := url.Values{}
+	query.Add("org", org)
+	query.Add("cursor", opts.Cursor)
+	if opts.Archived != nil {
+		query.Add("archived", strconv.FormatBool(*opts.Archived))
+	}
+
+	resp, err := c.sendRequest(ctx, http.MethodGet, "/api/v3/workspaces", query, nil)
+	if err != nil {
+		return nil, "", err
+	}
+	defer safeClose(resp.Body)
+
+	var result api.WorkspacePage
+	if err := parseResponse(resp, &result); err != nil {
+		return nil, "", err
+	}
+
+	return result.Data, result.NextCursor, nil
 }
 
 // Workspace gets a handle for a workspace by name or ID. The returned handle is
@@ -57,6 +96,146 @@ func (h *WorkspaceHandle) Get(ctx context.Context) (*api.Workspace, error) {
 	}
 
 	return &workspace, nil
+}
+
+type ListDatasetOptions struct {
+	Cursor        string
+	Archived      *bool
+	ResultsOnly   *bool
+	CommittedOnly *bool
+}
+
+func (h *WorkspaceHandle) Datasets(
+	ctx context.Context,
+	opts *ListDatasetOptions,
+) ([]api.Dataset, string, error) {
+	if opts == nil {
+		opts = &ListDatasetOptions{}
+	}
+
+	query := url.Values{}
+	query.Add("cursor", opts.Cursor)
+	if opts.Archived != nil {
+		query.Add("archived", strconv.FormatBool(*opts.Archived))
+	}
+	if opts.ResultsOnly != nil {
+		query.Add("results", strconv.FormatBool(*opts.ResultsOnly))
+	}
+	if opts.CommittedOnly != nil {
+		query.Add("committed", strconv.FormatBool(*opts.CommittedOnly))
+	}
+
+	path := path.Join("/api/v3/workspaces", h.id, "datasets")
+	resp, err := h.client.sendRequest(ctx, http.MethodGet, path, query, nil)
+	if err != nil {
+		return nil, "", err
+	}
+	defer safeClose(resp.Body)
+
+	var result api.DatasetPage
+	if err := parseResponse(resp, &result); err != nil {
+		return nil, "", err
+	}
+
+	return result.Data, result.NextCursor, nil
+}
+
+type ListExperimentOptions struct {
+	Cursor   string
+	Archived *bool
+}
+
+func (h *WorkspaceHandle) Experiments(
+	ctx context.Context,
+	opts *ListExperimentOptions,
+) ([]api.Experiment, string, error) {
+	if opts == nil {
+		opts = &ListExperimentOptions{}
+	}
+
+	query := url.Values{}
+	query.Add("cursor", opts.Cursor)
+	if opts.Archived != nil {
+		query.Add("archived", strconv.FormatBool(*opts.Archived))
+	}
+
+	path := path.Join("/api/v3/workspaces", h.id, "experiments")
+	resp, err := h.client.sendRequest(ctx, http.MethodGet, path, query, nil)
+	if err != nil {
+		return nil, "", err
+	}
+	defer safeClose(resp.Body)
+
+	var result api.ExperimentPage
+	if err := parseResponse(resp, &result); err != nil {
+		return nil, "", err
+	}
+
+	return result.Data, result.NextCursor, nil
+}
+
+type ListGroupOptions struct {
+	Cursor   string
+	Archived *bool
+}
+
+func (h *WorkspaceHandle) Groups(
+	ctx context.Context,
+	opts *ListGroupOptions,
+) ([]api.Group, string, error) {
+	if opts == nil {
+		opts = &ListGroupOptions{}
+	}
+
+	query := url.Values{}
+	query.Add("cursor", opts.Cursor)
+	if opts.Archived != nil {
+		query.Add("archived", strconv.FormatBool(*opts.Archived))
+	}
+
+	path := path.Join("/api/v3/workspaces", h.id, "groups")
+	resp, err := h.client.sendRequest(ctx, http.MethodGet, path, query, nil)
+	if err != nil {
+		return nil, "", err
+	}
+	defer safeClose(resp.Body)
+
+	var result api.GroupPage
+	if err := parseResponse(resp, &result); err != nil {
+		return nil, "", err
+	}
+
+	return result.Data, result.NextCursor, nil
+}
+
+type ListImageOptions struct {
+	Cursor string
+}
+
+func (h *WorkspaceHandle) Images(
+	ctx context.Context,
+	opts *ListImageOptions,
+) ([]api.Image, string, error) {
+	if opts == nil {
+		opts = &ListImageOptions{}
+	}
+
+	query := url.Values{}
+	query.Add("cursor", opts.Cursor)
+
+	path := path.Join("/api/v3/workspaces", h.id, "images")
+	resp, err := h.client.sendRequest(ctx, http.MethodGet, path, query, nil)
+	if err != nil {
+		return nil, "", err
+	}
+	defer safeClose(resp.Body)
+
+	var result api.ImagePage
+	if err := parseResponse(resp, &result); err != nil {
+		return nil, "", err
+	}
+
+	return result.Data, result.NextCursor, nil
 }
 
 // SetName sets a workspace's name.
