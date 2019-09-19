bd99594bd1b1d5a32742d3332b35d4b92d95dd89
beakerbot <beakerbot@users.noreply.github.com>
2019-09-19 00:46:03 +0000

    Automated update to 0ee9a7208ab00797d9a2f99c74c6452c8f3efffe

--- api/auth.go
+++ api/auth.go
@@ -30,10 +30,35 @@ type PermissionSummary struct {
 	Authorizations map[string]Permission `json:"authorizations,omitempty"`
 }
 
+// WorkspacePermissionSummary aggregates permissions for a particular workspace. Some fields
+// may be omitted when viewed by a user with limited permissions.
+// TODO: After migrating permissions to be workspace-based, rename to PermissionSummary
+type WorkspacePermissionSummary struct {
+	// Authorization for the user issuing a request.
+	RequesterAuth Permission `json:"requesterAuth"`
+
+	// Default permissions granted on the object.
+	Public bool `json:"public,omitempty"`
+
+	// Mapping of additional permissions granted to each user, indexed by user ID.
+	Authorizations map[string]Permission `json:"authorizations,omitempty"`
+}
+
 // PermissionPatch describes transactional changes to a single object's permissions.
 type PermissionPatch struct {
 	// (optional) Default permission to apply to all users.
 	Default *Permission `json:"default,omitempty"`
+
+	// (optional) Mapping of additional permissions granted to each user. Set a
+	// user's permission to "none" to reset that user's authorization to default.
+	Authorizations map[string]Permission `json:"authorizations,omitempty"`
+}
+
+// WorkspacePermissionPatch describes transactional changes to a single workspace's permissions.
+// TODO: After migrating permissions to be workspace-based, rename to PermissionPatch
+type WorkspacePermissionPatch struct {
+	// (optional) Default permission to apply to all users.
+	Public *bool `json:"public,omitempty"`
 
 	// (optional) Mapping of additional permissions granted to each user. Set a
 	// user's permission to "none" to reset that user's authorization to default.
