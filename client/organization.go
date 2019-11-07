53d31ceb226f2c4ee6e676353332c13b7bde8acd
beakerbot <beakerbot@users.noreply.github.com>
2019-11-07 22:29:56 +0000

    Automated update to f49524d0235865a7d13c52888734a15033712821

--- client/organization.go
+++ client/organization.go
@@ -6,15 +6,35 @@ import (
 	"path"
 
 	"github.com/pkg/errors"
+
+	"github.com/beaker/client/api"
 )
+
+// ListMyOrgs lists all orgs in which the caller is a member. The caller's
+// account is inferred from the client's auth token.
+func (c *Client) ListMyOrgs(ctx context.Context) ([]api.Organization, error) {
+	resp, err := c.sendRequest(ctx, http.MethodGet, path.Join("/api/v3/user/orgs"), nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer safeClose(resp.Body)
+
+	var orgs api.OrganizationPage
+	if err := parseResponse(resp, &orgs); err != nil {
+		return nil, err
+	}
+
+	// Cursor is not populated; assume the page contains all results.
+	return orgs.Data, nil
+}
 
 // VerifyOrgExists validates existence of an organization.
 func (c *Client) VerifyOrgExists(ctx context.Context, org string) error {
 	resp, err := c.sendRequest(ctx, http.MethodGet, path.Join("/api/v3/orgs", org), nil, nil)
-	defer safeClose(resp.Body)
 	if err != nil {
 		return errors.WithMessage(err, "could not resolve organization "+org)
 	}
+	defer safeClose(resp.Body)
 
 	return errors.WithMessage(errorFromResponse(resp), org)
 }
