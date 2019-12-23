58caee5f059955f7ba14d7feb58945ba3ca3afb3
beakerbot <beakerbot@users.noreply.github.com>
2019-12-23 21:45:15 +0000

    Automated update to f74cbc90996022cbe9751e1562c4e3394f2838cf

--- api/cluster.go
+++ api/cluster.go
@@ -92,20 +92,6 @@ const (
 	ClusterFailed ClusterStatus = "failed"
 )
 
-// Deprecated. Use clusters/nodes instead.
-type Machine struct {
-	ID          string `json:"id"`
-	CPU         int    `json:"cpu"`
-	Memory      int    `json:"memory"`
-	NodeLabel   string `json:"nodeLabel"`
-	GPUCount    int    `json:"gpuCount,omitempty"`
-	GPUType     string `json:"gpuType,omitempty"`
-	GPULabel    string `json:"gpuLabel,omitempty"`
-	Preemptible bool   `json:"preemptible"`
-	Cost        int64  `json:"cost"`
-	IsActive    bool   `json:"isActive"`
-}
-
 // NodeSpec provides options to configure compute nodes.
 type NodeSpec struct {
 	CPUCount int    `json:"cpuCount,omitempty"`
