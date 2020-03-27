fb50efdcdefb1ff11710611e2a202357a373f239
beakerbot <beakerbot@users.noreply.github.com>
2020-03-27 23:33:24 +0000

    Automated update to cd928ce1eeb077e4886f568d7758e118b970165f

--- client/organization.go
+++ client/organization.go
@@ -5,8 +5,6 @@ import (
 	"net/http"
 	"path"
 
-	"github.com/pkg/errors"
-
 	"github.com/beaker/client/api"
 )
 
@@ -28,13 +26,92 @@ func (c *Client) ListMyOrgs(ctx context.Context) ([]api.Organization, error) {
 	return orgs.Data, nil
 }
 
-// VerifyOrgExists validates existence of an organization.
-func (c *Client) VerifyOrgExists(ctx context.Context, org string) error {
-	resp, err := c.sendRequest(ctx, http.MethodGet, path.Join("/api/v3/orgs", org), nil, nil)
+// OrgHandle provides an interface for organization APIs.
+type OrgHandle struct {
+	client *Client
+	ref    string
+}
+
+// Organization gets a handle for an organization by name or ID. The returned
+// handle is not guaranteed to be valid.
+func (c *Client) Organization(reference string) *OrgHandle {
+	return &OrgHandle{client: c, ref: reference}
+}
+
+// Get retrieves an organization's details.
+func (h *OrgHandle) Get(ctx context.Context) (*api.Organization, error) {
+	path := path.Join("/api/v3/orgs", h.ref)
+	resp, err := h.client.sendRequest(ctx, http.MethodGet, path, nil, nil)
 	if err != nil {
-		return errors.WithMessage(err, "could not resolve organization "+org)
+		return nil, err
 	}
 	defer safeClose(resp.Body)
 
-	return errors.WithMessage(errorFromResponse(resp), org)
+	var org api.Organization
+	if err := parseResponse(resp, &org); err != nil {
+		return nil, err
+	}
+
+	return &org, nil
+}
+
+// ListMembers retrieves an organization's details.
+// TODO: Make this return an iterator.
+func (h *OrgHandle) ListMembers(
+	ctx context.Context,
+	cursor string,
+) (users []api.UserDetail, next string, err error) {
+	path := path.Join("/api/v3/orgs", h.ref, "members")
+	resp, err := h.client.sendRequest(ctx, http.MethodGet, path, nil, nil)
+	if err != nil {
+		return nil, "", err
+	}
+	defer safeClose(resp.Body)
+
+	var members api.UserPage
+	if err := parseResponse(resp, &members); err != nil {
+		return nil, "", err
+	}
+
+	return members.Data, members.NextCursor, nil
+}
+
+// GetMember returns details about a specific membership, if it exists.
+func (h *OrgHandle) GetMember(ctx context.Context, account string) (*api.OrgMembership, error) {
+	path := path.Join("/api/v3/orgs", h.ref, "members", account)
+	resp, err := h.client.sendRequest(ctx, http.MethodGet, path, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer safeClose(resp.Body)
+
+	var member api.OrgMembership
+	if err := parseResponse(resp, &member); err != nil {
+		return nil, err
+	}
+
+	return &member, nil
+}
+
+// SetMember adds or updates the given account as a member of the org.
+// Role must be "admin" or "member".
+func (h *OrgHandle) SetMember(ctx context.Context, account string, role string) error {
+	path := path.Join("/api/v3/orgs", h.ref, "members", account)
+	resp, err := h.client.sendRequest(ctx, http.MethodPut, path, nil, nil)
+	if err != nil {
+		return err
+	}
+	defer safeClose(resp.Body)
+	return errorFromResponse(resp)
+}
+
+// RemoveMember removes the given account from the org.
+func (h *OrgHandle) RemoveMember(ctx context.Context, account string) error {
+	path := path.Join("/api/v3/orgs", h.ref, "members", account)
+	resp, err := h.client.sendRequest(ctx, http.MethodDelete, path, nil, nil)
+	if err != nil {
+		return err
+	}
+	defer safeClose(resp.Body)
+	return errorFromResponse(resp)
 }
