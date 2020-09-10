25a4b2a85203b9977299d08d1e9ed53ad2c58bd3
beakerbot <beakerbot@users.noreply.github.com>
2020-09-10 22:43:10 +0000

    Automated update to cb52705ce81746e9df5ff4da59b6127945ca673a

--- api/reports.go
+++ api/reports.go
@@ -6,6 +6,34 @@ import (
 	"github.com/shopspring/decimal"
 )
 
+// NodeUsageReport contains one series for each combination of values in the group by.
+type NodeUsageReport struct {
+	Totals NodeUsageInterval `json:"totals"`
+	Series []NodeUsageSeries `json:"series"`
+}
+
+// NodeUsageSeries reports the value of a NodeMetric over time.
+type NodeUsageSeries struct {
+	// Node ID is included if the data is grouped by node.
+	Node string `json:"node,omitempty"`
+
+	// Cluster name is included if the data is grouped by cluster.
+	Cluster string `json:"cluster,omitempty"`
+
+	// Environment (on-prem or cloud) is included if the data is grouped by environment.
+	Environment string `json:"environment,omitempty"`
+
+	Totals    NodeUsageInterval   `json:"totals"`
+	Intervals []NodeUsageInterval `json:"intervals"`
+}
+
+// NodeUsageInterval reports the value of a NodeMetric during a single time interval.
+type NodeUsageInterval struct {
+	Start time.Time `json:"start"`
+	End   time.Time `json:"end"`
+	Value float64   `json:"value"`
+}
+
 // UsageReport:
 // Deprecated: UsageReport - Reporting in general needs an improved API
 type UsageReport struct {
