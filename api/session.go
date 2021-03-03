42962f9b2297984d4cb507df3bfeb48e4d45e3df
beakerbot <beakerbot@users.noreply.github.com>
2021-03-03 23:13:13 +0000

    Automated update to 028a09bf513956fcb30d07eeb8b2a100d8476cfc

--- api/session.go
+++ api/session.go
@@ -4,9 +4,27 @@ package api
 type Session struct {
 	ID string `json:"id"`
 
+	// Node that the session is assigned to.
+	Node string `json:"node"`
+
 	// Limits describes resources assigned to this session.
 	Limits TaskResources `json:"limits"`
 
 	// State describes session status and progression.
 	State ExecutionState `json:"state"`
+}
+
+// SessionSpec defines a session.
+type SessionSpec struct {
+	// (required) ID of the node that the session is running on.
+	Node string `json:"node" yaml:"node"`
+}
+
+// SessionPatch updates a session.
+type SessionPatch struct {
+	// Limits updates the resources assigned to a session.
+	Limits *TaskResources `json:"limits"`
+
+	// State updates the session status and progression.
+	State *ExecutionState `json:"state"`
 }
