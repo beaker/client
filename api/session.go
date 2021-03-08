a2c76d2f95b4036049bf6b09e9c60d7a713daf25
beakerbot <beakerbot@users.noreply.github.com>
2021-03-08 20:28:02 +0000

    Automated update to a533c5ea8b3d997a7a99d1054bcd5f5bcba7641a

--- api/session.go
+++ api/session.go
@@ -2,13 +2,17 @@ package api
 
 // Session is an interactive Beaker session.
 type Session struct {
-	ID string `json:"id"`
+	// Identity
+	ID      string   `json:"id"`
+	Name    string   `json:"name"`
+	Cluster string   `json:"cluster"`
+	Author  Identity `json:"author"`
 
 	// Node that the session is assigned to.
 	Node string `json:"node"`
 
 	// Limits describes resources assigned to this session.
-	Limits TaskResources `json:"limits"`
+	Limits *TaskResources `json:"limits"`
 
 	// State describes session status and progression.
 	State ExecutionState `json:"state"`
@@ -18,6 +22,9 @@ type Session struct {
 type SessionSpec struct {
 	// (required) ID of the node that the session is running on.
 	Node string `json:"node" yaml:"node"`
+
+	// (optional) Name for the session.
+	Name string `json:"name" yaml:"name"`
 }
 
 // SessionPatch updates a session.
